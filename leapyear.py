8006cc7d915dab2843e2c8facbcf50e90c1b4602
maha98 <42862532+maha98@users.noreply.github.com>
2018-09-02 14:04:43 +0530

    Create leapyear.py

---
+++ leapyear.py
@@ -0,0 +1,11 @@
+year=int(raw_input())
+if (year%4==0):
+  if(year%100==0):
+    if(year%400==0):
+      print ("yes")
+    else:
+      print("no")
+  else:
+    print("yes")
+else:
+  print("no")    lea
