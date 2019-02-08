27c8aa5541efc57cbe5814b8e0c1e7a790b26ac4
maha98 <42862532+maha98@users.noreply.github.com>
2019-02-08 12:20:48 +0700

    Update larger.py

--- larger.py
+++ larger.py
@@ -1,9 +1,9 @@
-a=int(raw_input())
-b=int(raw_input())
-c=int(raw_input())
-if ((a>b) and (a>c)):
- print a
-elif ((b>a) and (b>c)):
-  print b
+a1=int(input())
+b1=int(input())
+c1=int(input())
+if ((a1>b1) and (a1>c1)):
+     print (a1)
+elif ((b1>a1) and (b1>c1)):
+    print (b1)
 else :
-  print c   
+     print (c1)
